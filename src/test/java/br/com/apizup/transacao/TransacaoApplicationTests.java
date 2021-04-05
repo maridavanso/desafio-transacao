d2aad1d10d7abdac95c0f83710037aac387ec821
maridavanso <mariana.regalau@gmail.com>
2021-04-05 17:48:35 -0300

    desafio-transacao

---
+++ src/test/java/br/com/apizup/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apizup.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
